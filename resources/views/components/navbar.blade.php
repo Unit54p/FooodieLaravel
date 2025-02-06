e48eccdf1fc20b48ed03ddbe26159d187c97490a
Surya <suryaarioprrama@gmail.com>
2025-02-06 13:16:11 +0700

    fix nav bug

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,26 +1,35 @@
 <link rel="stylesheet" href="{{ asset('css/AboutUs.css') }}">
 <nav class="px-10 py-5  flex justify-between align-middle ">
-    <ul class="flex justify-start ">
+    <ul class="flex justify-start items-center">
         {{--
         ctrl shift f untuk mencari sesuatu secara global
         misal disini lupa letak navLi dikelas mana, maka cari menggunakan cara tersebut
         --}}
-        <li class="navLi {{ Request::is('Home') ? 'active' : '' }}"> <a href="/Home">Home</a></li>
-        <li class="navLi {{ Request::is('foods') ? 'active' : '' }}"> <a href="{{ route('foodsName') }}">Foods</li>
-        <li class="navLi {{ Request::is('drinks') ? 'active' : '' }}"> <a href="/drinks">Drinks</li>
-        <li class="navLi {{ Request::is('apps') ? 'active' : '' }}"> <a href="/apps">Apps</li>
-        <li class="navLi {{ Request::is('about') ? 'active' : '' }}">
-            <a href="/about">About Us</a>
+        <li>
+            <a class="navLi  {{ Request::is('Home') ? 'active' : '' }}"" href=" /Home">Home</a>
+        </li>
+        <li>
+            <a class="navLi {{ Request::is('foods') ? 'active' : '' }}"" href=" {{ route('foodsName') }}">Foods</a>
+        </li>
+        <li>
+            <a class=" navLi {{ Request::is('drinks') ? 'active' : '' }}" href="/drinks">Drinks</a>
+        </li>
+        <li>
+            <a class="navLi {{ Request::is('apps') ? 'active' : '' }}"" href=" /apps">Apps</a>
+        </li>
+        <li>
+            <a class="navLi {{ Request::is('about') ? 'active' : '' }}"" href=" /about">About Us</a>
         </li>
     </ul>
-    @auth
-        <div class="flex flex-row userProfile gap-4">
-            {{-- <a href="{{ route('editUserView', Auth::user()->id) }}" class="navLi">Welcome, {{ Auth::user()->name }}</a>
-            --}}
-            <img src="{{ asset('img/cart.svg') }}" alt="" style="width: 55%" class="hover:cursor-pointer hover:scale-110 ">
-            <button onclick="window.location='{{ route('editUserView', ['id' => Auth::user()->id]) }}'" class="img-btn">
+    <div class="flex justify-end flex-row userProfile gap-4">
+        {{-- <a href="{{ route('editUserView', Auth::user()->id) }}" class="navLi">Welcome, {{ Auth::user()->name }}</a>
+        --}}
+        @auth
+            <img src="{{ asset('img/cart.svg') }}" alt="" style="width: 12%" class="hover:cursor-pointer hover:scale-110 ">
+            <button onclick="window.location='{{ route('editUserView', ['id' => Auth::user()->id]) }}'" style="width: 12%"
+                class="btn_img ">
                 <img src="{{ Storage::url(Auth::user()->imgProfile) }}" alt="Edit Profile" class="imgUser">
-            </button>
-        </div>
-    @endauth
+        @endauth
+        </button>
+    </div>
 </nav>
